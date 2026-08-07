01c7329517893a6e21262c51e3e9cace0f3fd921
Carl Jasper Ramos <zoraao79@gmail.com>
2026-08-07 15:52:40 +0800

    feat: setup and install shadcn ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from "./components/ui/button"
+
 const App = () => {
   return (
     <div>
-      <h1 className="text-red-400 bg-red-900">Hello World</h1>
+      <Button>Testing</Button>
     </div>
   )
 }
